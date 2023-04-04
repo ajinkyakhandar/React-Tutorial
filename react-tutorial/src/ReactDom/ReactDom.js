79e6b0300524e6453470932c8cec5db5d4e0121b
ajinkyakhandar <khandarajinkya@gmail.com>
2023-04-04 14:52:12 +0530

    React Without JSX - attribute

--- react-tutorial/src/ReactDom/ReactDom.js
+++ react-tutorial/src/ReactDom/ReactDom.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const ReactDom = () => {
-  return React.createElement("div", null, "Hello World");
+  return React.createElement("div", {style: {color: "black", backgroundColor: "#87CEEB", padding:  "10px"}}, "Hello World");
 };
 
 export default ReactDom;
